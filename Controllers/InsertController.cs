04f2c0ee95fdbc468029dba36ba27f0d7fa5b47f
PSb19 <33777820+PSb19@users.noreply.github.com>
2023-11-20 21:49:49 +0100

    Rethink ComplexValue, stub insert and get methods

--- Controllers/InsertController.cs
+++ Controllers/InsertController.cs
@@ -1,16 +1,37 @@
+using calculator40k.Models;
 using dmgCalculatorAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dmgCalculatorAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/Insert")]
 public class InsertController : ControllerBase{
 
-    private ApiContext _context;
+    private readonly ApiContext _context;
 
     public InsertController(ApiContext context){
         _context = context;
     }
 
+    [HttpGet("/return")]
+    public async Task<ActionResult<IEnumerable<Unit>>> GetAllUnits(){
+        var units = await _context.Units.ToListAsync();
+
+        if(units != null){
+            return units;
+        }
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Unit>> AddNewUnit(Unit unit){
+        //check unit validity
+        return unit;
+        //TODO: Make this work - every class saved to db has to have key etc
+        _context.Units.Add(unit);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
 }
--- Models/ComplexValue.cs
+++ Models/ComplexValue.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace calculator40k.Models
 {
+    //Troubles while using database
     public class ComplexValue
     {
         public string? Value {get; set;}
